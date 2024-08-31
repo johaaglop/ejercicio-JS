5030c2054abea0f045cafb4ed8e01043325880af
johaaglop <johana.aguilar04@gmail.com>
2024-08-31 05:37:47 -0600

    pruebas java

---
+++ js/restaurante.js
@@ -0,0 +1,10 @@
+var totalComida = 2800 + 1300;
+var totalBebida = 900 + 650;
+
+var total = totalComida + totalBebida;
+
+console.log("El total de comida es: " + totalComida)
+console.log("El total de las bebidas es: " + totalBebida)
+console.log("El total de la cuenta es: " + total)
+
+
